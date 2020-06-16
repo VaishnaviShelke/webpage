82881e3ed883777878b08d65afb51c70bfe7e7ad
VaishnaviShelke <64678957+VaishnaviShelke@users.noreply.github.com>
2020-06-16 14:08:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,7 @@
 <h1>AWS & Terraform</h1>
   <pre>
 <?php
-$file = file_get_contents('url.txt');
-echo $file;
-echo '<img src="'.$file.'"  width="200" height="200">';
+print `ifconfig`;
 ?>
 </pre>
 
