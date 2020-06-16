c7d415d1e094705c0a37eb5877672999c45f71c7
VaishnaviShelke <64678957+VaishnaviShelke@users.noreply.github.com>
2020-06-16 11:39:01 +0530

    commit

---
+++ index.php
@@ -0,0 +1,13 @@
+
+<html>
+<h1> AWS & Terraform </h1>
+  
+  <pre>
+  <?php
+  $file = file_get_contents('path.txt');
+  echo $file;
+  echo 'img src ="'.$file.'" width="200" height="200" >';
+  ?>
+  </pre>
+  
+</html>
