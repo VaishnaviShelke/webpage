550a356476013076570604902fe52037ad0c9a07
VaishnaviShelke <64678957+VaishnaviShelke@users.noreply.github.com>
2020-07-25 11:36:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,15 +2,14 @@
 <html>
 <body bgcolor="gray">
 
-<h1>AWS & Terraform</h1>
+<h1>AWS & Terraform & EKS file </h1>
   <pre>
 <?php
 $file = file_get_contents('url.txt');
-echo $file;
+//echo $file;
 echo '<img src="'.$file.'"  width="200" height="200">';
 
 ?>
 </pre>
-
 </body>
 </html>
